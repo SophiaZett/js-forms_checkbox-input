ca70dacf13283369c5216d48735bc581bfaaa8c9
Sophia S. <sophia0791@icloud.com>
2023-12-12 16:22:10 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,11 +13,22 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+tosCheckbox.addEventListener("change", () => {
+  if (tosCheckbox.checked) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
+})
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+if (tosCheckbox.checked) {
+  success.style.visibility = "visible";
+  return;
+}
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
